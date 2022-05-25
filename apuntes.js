2cae566dbd4b635cbe3e9fb05e4a53a1fec50531
Wilber <wlunaj111@gmail.com>
2022-05-25 15:15:18 -0400

    modificacion 1

--- apuntes.js
+++ apuntes.js
@@ -1,2 +1,9 @@
- //instalar angular cli de manera global 
- npm i @angular/cli -g
+ //Install the Angular CLI 
+ npm install -g @angular/cli
+
+ //Create a workspace and initial application
+ ng new my-app
+
+//Run the application
+cd my-app
+ng serve --open
