c392bada38f973758d65b9fdfd034c02cc175bef
wlunaj111 <58739856+wlunaj111@users.noreply.github.com>
2022-12-01 09:26:07 -0500

    .
    .

--- analitics.js
+++ analitics.js
@@ -1,1 +1,31 @@
-Cookies e Identificación de Usuario
+/*Cookies e Identificación de Usuario
+
+Para que Google Analytics determine que dos hits distintos
+pertenecen al mismo usuario, se debe enviar un identificador único, asociado con ese usuario en particular, con cada hit.
+
+Te recomiendo que tu servidor y tu cliente negocien un identificador y que éste sea almacenado en algún lugar en el que 
+pueda persistir, tanto en el cliente como en el servidor.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
